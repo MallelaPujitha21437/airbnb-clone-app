c742a0aefca1b9b3125b1ed006e132c7ebefe249
Mallela.Pujitha <mallelapujitha0508@gmail.com>
2026-09-25 14:54:32 +0530

    Allow esbuild install script for Vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,12 @@ import cors from "cors";
 import routes from "./routes/listingRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorHandler.js";
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 app.use("/api", routes);
 app.use(notFound);
 app.use(errorHandler);
-app.listen(PORT, () =>
-  console.log(`Backend running at http://localhost:${PORT}`),
-);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
